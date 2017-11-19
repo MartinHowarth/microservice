143275c27ac27f97c7af997801e0cd456aeafa78
MartinHowarth <howarth.martin@gmail.com>
2017-11-19 13:13:14 +0000

    hello world test k8s dns call

--- microservice/examples/hello_world.py
+++ microservice/examples/hello_world.py
@@ -2,5 +2,18 @@ from microservice.core.decorator import microservice
 
 
 @microservice
-def hello_world(*args, **kwargs):
+def hello_world(*args, stop=False):
+    if stop:
+        return "Hello, world - final!"
+
+    import requests
+    req = requests.get(
+        "http://microservice-examples-hello-world-hello-world.pycroservices:5000/microservice.examples.hello_world.hello_world",
+        json={
+            '_args': tuple(),
+            '_kwargs': {'stop': True},
+        }
+    )
+    print(req)
+    print(req.text)
     return "Hello, world!"
